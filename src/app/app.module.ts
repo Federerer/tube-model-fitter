883c97bdb208770e8e301185b0419d2e714e1766
agas <08fedra@interia.pl>
2022-03-16 22:02:06 +0100

    resize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,20 @@ import { AppComponent } from './app.component';
 import { CurveChartComponent } from './components/curve-chart/curve-chart.component';
 import { OnResizeDirective } from './directives/on-resize.directive';
 
+import { ResizableModule } from 'angular-resizable-element';
+import { ResizeBoxComponent } from './components/resize-box/resize-box.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     CurveChartComponent,
-    OnResizeDirective
+    OnResizeDirective,
+    ResizeBoxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ResizableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
