c9ed0540b8486d35cc29d3b4443e006961fcde71
agas <08fedra@interia.pl>
2022-03-16 22:03:31 +0100

    paste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { DempwolfZolzer } from './model/models/dempwolf-zolzer';
 import { Koren } from './model/models/koren';
 import { TriodeModel } from './model/triode-model';
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public model: TriodeModel;
+  public model: Koren;
+
+  public imageUrl: SafeUrl = "";
 
   /**
    *
    */
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
     this.model = new Koren();
     // this.model = new DempwolfZolzer();
+  }
+
+  public onChange(val: any) {
+    this.model.ex = Number(val);
+  }
+
+  onPaste(e: any) {
+    console.log(e);
+    const items = (e.clipboardData || e.originalEvent.clipboardData).items;
+    let blob = null;
+    for (const item of items) {
+      if (item.type.indexOf('image') === 0) {
+        blob = item.getAsFile();
+        console.log(blob);
+        this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(
+          URL.createObjectURL(blob)
+        );
+      }
+    }
   }
 }
